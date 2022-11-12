717345268762cc9fa93c4219d0da4326be497eb8
Bernhard Glück <business@bernhardglueck.com>
2022-11-12 21:09:39 +0100

    Added R2D_Plus

--- models/classification/video/video_classification_model_factories.py
+++ models/classification/video/video_classification_model_factories.py
@@ -1,7 +1,8 @@
 from typing import Dict
 
 from torch.nn import Module
-from torchvision.models.video import r3d_18, mvit_v2_s, s3d
+from torchvision.models.video import r3d_18, mvit_v2_s, s3d, r2plus1d_18
+from pytorchvideo import models
 
 from models.classification.classification_model_factories import register_classification_model
 from models.classification.classification_module import ClassificationModule
@@ -27,12 +28,25 @@ def create_s3d(configuration_reader: ConfigurationReader, model_configuration: V
     return s3d(weights=None, num_classes=num_classes)
 
 
+def create_mvit(configuration_reader: ConfigurationReader, model_configuration: VideoClassificationModelConfiguration,
+                num_classes: int):
+    return models.create_multiscale_vision_transformers(
+        spatial_size=(model_configuration.clip.height, model_configuration.clip.width),
+        temporal_size=model_configuration.clip.get_frame_count(), head_num_classes=num_classes)
+
+
 def create_mvit_v2_s(configuration_reader: ConfigurationReader,
                      model_configuration: VideoClassificationModelConfiguration, num_classes: int) -> Module:
     return mvit_v2_s(weights=None)
 
 
+def create_r2dplus(configuration_reader: ConfigurationReader,
+                   model_configuration: VideoClassificationModelConfiguration, num_classes: int) -> Module:
+    return r2plus1d_18(weights=None, num_classes=num_classes)
+
+
 def register_all_video_classification_models():
     register_video_classification_model("r3d_18", create_r3d_18)
-    register_video_classification_model("mvit_v2_s", create_mvit_v2_s)
+    register_video_classification_model("mvit", create_mvit)
     register_video_classification_model("s3d", create_s3d)
+    register_video_classification_model("r2d_plus", create_r2dplus)
