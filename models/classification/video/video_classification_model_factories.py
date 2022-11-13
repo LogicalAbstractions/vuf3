048f0aafb143c8a699a6f294249b45d70f6d7bcd
Bernhard Glück <business@bernhardglueck.com>
2022-11-13 01:45:34 +0100

    Added more experiments

--- models/classification/video/video_classification_model_factories.py
+++ models/classification/video/video_classification_model_factories.py
@@ -35,6 +35,12 @@ def create_mvit(configuration_reader: ConfigurationReader, model_configuration: 
         temporal_size=model_configuration.clip.get_frame_count(), head_num_classes=num_classes, depth=8)
 
 
+def create_slowfast(configuration_reader: ConfigurationReader,
+                    model_configuration: VideoClassificationModelConfiguration,
+                    num_classes: int):
+    return models.create_slowfast(model_num_class=num_classes)
+
+
 def create_mvit_v2_s(configuration_reader: ConfigurationReader,
                      model_configuration: VideoClassificationModelConfiguration, num_classes: int) -> Module:
     return mvit_v2_s(weights=None)
@@ -50,3 +56,4 @@ def register_all_video_classification_models():
     register_video_classification_model("mvit", create_mvit)
     register_video_classification_model("s3d", create_s3d)
     register_video_classification_model("r2d_plus", create_r2dplus)
+    register_video_classification_model("slowfast", create_slowfast)
